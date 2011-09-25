c02ef212b68c1904df672392bd3e79f19d0f2a8a
Yorick Peterse <yorickpeterse@gmail.com>
2011-09-25 22:06:23 +0200

    Merged Zen::Theme and Zen::Theme::Base.
    Similar to Zen::Package Zen::Theme and Zen::Theme::Base have been merged into a
    single class (Zen::Theme). I also changed the attribute names in a similar
    fashion to Zen::Package. For now Zen::Theme is backwards compatible but I'll
    remove all the aliases in Zen 0.3.
    
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/zen/theme.rb
+++ lib/zen/theme.rb
@@ -1,4 +1,3 @@
-require __DIR__('theme/base')
 require 'pathname'
 
 #:nodoc:
@@ -10,12 +9,12 @@ module Zen
   # the developer updates these paths manually. However, themes can set the
   # following paths:
   #
-  # * template_dir: the directory where all Liquid templates are located,
-  #   always required.
-  # * partial_dir: the directory where all Liquid partials are located.
-  # * public_dir: the public directory containing assets such as CSS and
-  #   Javascript files.
-  # * migration_dir: themes can use migrations to automatically add the required
+  # * templates: the directory where all Liquid templates are located, always
+  #   required.
+  # * partials: the directory where all Liquid partials are located.
+  # * public: the public directory containing assets such as CSS and Javascript
+  #   files.
+  # * migrations: themes can use migrations to automatically add the required
   #   fields to the database, this setting should point to the directory where
   #   all migrations are located.
   #
@@ -30,13 +29,39 @@ module Zen
   #
   #     Zen::Theme.add do |theme|
   #       theme.author = 'Yorick Peterse'
-  #       theme.name   = 'default'
+  #       theme.name   = :default
   #     end
   #
   # @author Yorick Peterse
   # @since  0.2.4
   #
-  module Theme
+  class Theme
+    include Zen::Validation
+
+    # The name of the theme
+    attr_reader :name
+
+    # The author of the theme
+    attr_accessor :author
+
+    # A small description of the theme
+    attr_accessor :about
+
+    # The URL to the theme's homepage
+    attr_accessor :url
+
+    # Path to the directory containing all the templates.
+    attr_accessor :templates
+
+    # Path to the directory containing all template partials.
+    attr_accessor :partials
+
+    # Path to the directory containing all migrations for the theme.
+    attr_accessor :migrations
+
+    # Path to the public directory containing assets and such.
+    attr_reader :public
+
     ##
     # Hash containing all registered themes. The keys are the names of the
     # themes and the values instances of Zen::Theme::Base.
@@ -54,25 +79,11 @@ module Zen
     # @since  0.2.4
     #
     def self.add
-      theme = Base.new
+      theme = self.new
 
       yield theme
 
-      # Validate the theme
       theme.validate
-      theme.name = theme.name.to_sym
-
-      # Do we have a public directory?
-      if theme.respond_to?(:public_dir) and !theme.public_dir.nil?
-        if !Ramaze.options.publics.include?(theme.public_dir)
-          # Generate a relative path from ROOT to the theme
-          to   = Pathname.new(theme.public_dir)
-          from = Pathname.new(Zen.root)
-          dir  = to.relative_path_from(from.realpath).to_s
-
-          Ramaze.options.publics.push(dir)
-        end
-      end
 
       Registered[theme.name] = theme
     end
@@ -94,5 +105,76 @@ module Zen
 
       return Registered[name]
     end
+
+    ##
+    # Sets the name of the theme.
+    #
+    # @author Yorick Peterse
+    # @since  0.2.9
+    # @param  [#to_sym] name The name of the theme as a symbol.
+    #
+    def name=(name)
+      @name = name.to_sym
+    end
+
+    ##
+    # Sets the public directory of the theme.
+    #
+    # @author Yorick Peterse
+    # @since  0.2.9
+    # @param  [String] dir The public directory.
+    #
+    def public=(dir)
+      @public = dir
+
+      if !Ramaze.options.publics.include?(@public)
+        # Generate a relative path from ROOT to the theme
+        to   = Pathname.new(@public)
+        from = Pathname.new(Zen.root)
+        dir  = to.relative_path_from(from.realpath).to_s
+
+        Ramaze.options.publics.push(dir)
+      end
+    end
+
+    ##
+    # Validates all attributes of this class.
+    #
+    # @author Yorick Peterse
+    # @since  0.2.5
+    #
+    def validate
+      validates_presence([:name, :author, :about, :templates])
+
+      validates_filepath(:templates)
+
+      [:partials, :public, :migrations].each do |m|
+        if !send(m).nil?
+          validates_filepath(m)
+        end
+      end
+
+      # Check if the theme hasn't already been registered
+      if ::Zen::Theme::Registered.key?(name.to_sym)
+        raise(
+          ::Zen::ValidationError,
+          "The theme #{name} has already been registered."
+        )
+      end
+    end
+
+    # Aliases to keep Zen backwards compatible. These aliases will be removed in
+    # Zen 0.3.
+    alias :template_dir  :templates
+    alias :template_dir= :templates=
+
+    alias :partial_dir   :partials
+    alias :partial_dir=  :partials=
+
+    alias :migration_dir  :migrations
+    alias :migration_dir= :migrations=
+
+    alias :public_dir  :public
+    alias :public_dir= :public=
   end # Theme
 end # Zen
